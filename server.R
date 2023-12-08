3e41f7e3f999f971f1d1271efed516c65a783283
Sofia Octaviana <111562104+sofiaoctaviana@users.noreply.github.com>
2023-12-08 09:48:28 +0700

    Update server.R
    plot dari data upload mandiri belum muncul

--- server.R
+++ server.R
@@ -6,121 +6,108 @@ library(xts)
 library(forecast)
 library(lubridate)
 library(readxl)
+library(plotly)
+library(fpp2)
 
+# ===================== SERVER ===================== #
 
 shinyServer(function(input, output) {
+  #First tab 
+  # ------------ Definisi
   
-  # Live Chart
-  output$myPlot <- renderPlot({
+  #Second tab
+  # ------------- Time Series Plot
+  
+  # ------------- Menu Upload Mandiri
+  data <- reactiveVal(NULL)
+  observeEvent(input$chosen_file, {
+    req(input$chosen_file)
     
-    # reading in the data from the API
-    data_kurs <- read_xlsx("C:/Users/Sofia/Downloads/kurs.xlsx")
-    
-    # need to convert the dates to Date class in order to use scale_x_data
-    Date_kurs_class <- as.Date(data_kurs$Tanggal)
-    
-    if(input$type == "Data Trend"){
-      
-      ggplot(data_kurs, aes(x = Date_kurs_class, y = Terakhir, group = 1)) +
-        geom_line(color = "DarkBlue") + 
-        xlab("Periode") +
-        ylab("Kurs Dollar") +
-        # spreads out the x values and lanels them by their respective months and dates
-        scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-        # makes the labels slanted by 45 degrees
-        theme_classic() +
-        theme(axis.text.x = element_text(angle = -45, vjust = 0)) +
-        ggtitle("Kurs Rupiah Terhadap Dollar")
-    } else if(input$type == "Input Mandiri"){
-      
-      ggplot(data_kurs, aes(x = Date_kurs_class, y = Terakhir, group = 1)) +
-        geom_line(color = "DarkGreen") + 
-        xlab("Periode") +
-        ylab("Kurs Dollar") +
-        # spreads out the x values and lanels them by their respective months and dates
-        scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-        # makes the labels slanted by 45 degrees
-        theme_classic() +
-        theme(axis.text.x = element_text(angle = -45, vjust = 0)) +
-        ggtitle("Kurs Rupiah Terhadap Dollar")
-      
-    } else if(input$type == 3){
-      
-      ggplot(data_kurs, aes(x = Date_kurs_class, y = Terakhir, group = 1)) +
-        geom_line(color = "DarkBlue") + 
-        xlab("Periode") +
-        ylab("Kurs Dollar") +
-        # spreads out the x values and lanels them by their respective months and dates
-        scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-        # makes the labels slanted by 45 degrees
-        theme_classic() +
-        theme(axis.text.x = element_text(angle = -45, vjust = 0)) +
-        ggtitle("Kurs Rupiah Terhadap Dollar")
-    } else if(input$type == 4){
-      
-      ggplot(data_kurs, aes(x = Date_kurs_class, y = Terakhir, group = 1)) +
-        geom_line(color = "DarkBlue") + 
-        xlab("Periode") +
-        ylab("Kurs Dollar") +
-        # spreads out the x values and lanels them by their respective months and dates
-        scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-        # makes the labels slanted by 45 degrees
-        theme_classic() +
-        theme(axis.text.x = element_text(angle = -45, vjust = 0)) +
-        ggtitle("Kurs Rupiah Terhadap Dollar")
-    } else if(input$type == 5){
-      
-      ggplot(data_kurs, aes(x = Date_kurs_class, y = Terakhir, group = 1)) +
-        geom_line(color = "DarkBlue") + 
-        xlab("Periode") +
-        ylab("Kurs Dollar") +
-        # spreads out the x values and lanels them by their respective months and dates
-        scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-        # makes the labels slanted by 45 degrees
-        theme_classic() +
-        theme(axis.text.x = element_text(angle = -45, vjust = 0)) +
-        ggtitle("Kurs Rupiah Terhadap Dollar")
-    }
+    # Membaca file CSV
+    data_read <- read.csv(input$chosen_file$datapath)
+    data(data_read)
+    })
+  
+  
+  # -------------- Menampilkan Plot Upload Mandiri
+  observeEvent(input$loadBtn, {
+    req(data())
+    output$uploadData <- renderPlotly({
+      req(data())
+      if (!is.null(data()$XColumn) && !is.null(data()$YColumn)) {
+        plot_ly(data(), x = ~Date, y = ~General, type = 'scatter', mode = 'lines')
+      } else {
+        plot_ly(data()) 
+      }
+    })
   })
   
-  # second plot
-  # must use renderDygraph instead of renderPlot
-  output$dygraph <- renderDygraph({
+  # --------------- Pola Data Time Series
+  
+  output$myPlot <- renderPlotly({
+    # ------------ Memanggil Data
+    data_kurs <- read_xlsx("C:/Users/Sofia/Downloads/kurs.xlsx")
+    data_penduduk <- read_xlsx("C:/Users/Sofia/OneDrive/Documents/Semester 3/Eksplorasi dan Visualisasi Data/Dashboard EVD/penduduk.xlsx")
     
-    data <- get_data(from = input$start2, to = input$end2, stock_symbol = input$company2, view_type = input$type2, api_key = "Q86JK3PHWQIELR2K")
+      if(input$tipe == "Data Trend"){
+      p1 <- plot_ly(data_penduduk, x= data_penduduk$Tanggal, y = data_penduduk$Jumlah, type = 'scatter', mode = 'lines', marker = list(color = 'red'))
+      p1 <- p1 %>% layout(title = "Jumlah Penduduk Dunia",
+                          xaxis = list(title = "Tahun"),
+                          yaxis = list (title = "Jumlah Penduduk"))
+      bins <- seq(min(data_penduduk$Tanggal), max(data_penduduk$Tanggal), length.out <- input$slider.n+1)
+      ggplotly(p1, x = ~Tanggal, y = ~Jumlah, breaks = bins)
     
-    new_dat <- data %>%
-      arrange(Dates)
+    }  
+      else if(input$tipe == "Data Musiman"){
+      plot_ly(data_kurs, x= data_kurs$Tanggal, y = data_kurs$Terakhir, type = 'scatter', mode = 'lines')
+      
+    } else if(input$tipe == "Data Siklus"){
+      plot_ly(data_temperatur, x= data_temperatur$AirPassengers, y = data_kurs$Terakhir, type = 'scatter', mode = 'lines')
+      
+    } else if(input$tipe == "Data Fluktuatif"){
+      p4 <- plot_ly(data_kurs, x= data_kurs$Tanggal, y = data_kurs$Terakhir, type = 'scatter', mode = 'lines', marker = list(color = 'blue'))
+      p4 <- p4 %>% layout(title = "Kurs Rupiah Terhadap Dolar",
+                          xaxis = list(title = "Tahun"),
+                          yaxis = list (title = "Kurs Dollar"))
+      ggplotly(p4, smoothnes = input$slider.n)  #(slider smooth trendnya masih tidak berfungsi)
+    }
     
-    start_date <- as.Date(new_dat[1,1])
-    end_date <- as.Date(new_dat[length(new_dat$Dates), 1])
+  })
+  
+  #----------------- Smooth Trend  (smooth trendnya masih tidak berfungsi)
+  
+  output$PolaSmooth <- renderPlotly({
+    data_kurs <- read_xlsx("C:/Users/Sofia/Downloads/kurs.xlsx")
+    scatter.smooth(x = data_kurs$Tanggal, y = data_kurs$Terakhir, type = 'o', pch=20, 
+                   lpars = list(col = 'red', lwd = 2), 
+                   main = 'Kurs Rupiah Terhadap Dollar',
+                   xlab = 'Tahun',
+                   ylab = 'Kurs')
     
-    inds <- seq(start_date, end_date, by = "day")
+  })
+  
+  
+  # Third tab
+  # -------------- Forecast Plot
+  
+  output$trendPlot <- renderPlot({
+    penduduk <- read_xlsx("C:/Users/Sofia/OneDrive/Documents/Semester 3/Eksplorasi dan Visualisasi Data/Dashboard EVD/penduduk.xlsx")
+    penduduk <- penduduk[c("Tanggal", "Jumlah")]
+    penduduk$Tanggal <- as.Date(penduduk$Tanggal)
     
-    uni_data <- new_dat %>%
-      select(Stock_data)
+    require(gridExtra)
+    p1 <- autoplot(ts(penduduk[input$n[1]:input$n[2], "Jumlah"])) +
+      ggtitle("Data Penduduk Dunia")
     
-    zoo.obj <- zoo(uni_data, inds)
+    end = dim(penduduk)[1]
+    start = end - 50
     
-    model = auto.arima(zoo.obj)
+    mod <- auto.arima(penduduk[start : end, "Jumlah"])
+    data <- forecast(mod, arm = input$arm)
+    p2 <- autoplot(forecast(mod, arm = input$arm)) + 
+      ggtitle("Meramalkan data 5 tahun kedepan berdasarkan data 50 tahun sebelumnya")
     
-    predict = forecast(model, h = input$predict) # number of days predicted
-    
-    predict %>%
-      {cbind(actuals=.$x, forecast_mean=.$mean)} %>%
-      dygraph()
-    
-    predict %>%
-      {cbind(actuals=.$x, forecast_mean=.$mean,
-             lower_95=.$lower[,"95%"], upper_95=.$upper[,"95%"],
-             lower_80=.$lower[,"80%"], upper_80=.$upper[,"80%"])} %>%
-      dygraph() %>%
-      dySeries("actuals", color = "black") %>%
-      dySeries(c("lower_80", "forecast_mean", "upper_80"),
-               label = "80%", color = "blue") %>%
-      dySeries(c("lower_95", "forecast_mean", "upper_95"),
-               label = "95%", color = "blue") %>%
-      dyOptions(drawGrid = input$showgrid)
+    grid.arrange(p1, p2, ncol=1)
   })
   
 })
